98e4d3555984c4b605219eb30d28e63b082814cc
mingo99 <2839131656@qq.com>
2024-08-20 00:32:43 +0800

    inst: plugins for editor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,6 +9,10 @@ return {
         sidebars = "transparent", -- style for sidebars, see below
         floats = "transparent", -- style for floating windows
       },
+      -- transparent for statusline
+      on_colors = function(colors)
+        colors.bg_statusline = colors.none
+      end,
     },
   },
 }
