edef89d368e9e6707475a7d8ea6c9e3d970f4c33
mingo99 <2839131656@qq.com>
2024-08-25 23:46:39 +0800

    conf: transparent for window of completion info

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {
+      window = {
+        completion = {
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+          winblend = 0,
+          col_offset = -3,
+          side_padding = 0,
+          border = "rounded",
+          -- scrollbar = true,
+        },
+        documentation = {
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+          winblend = 0,
+          border = "rounded",
+          -- scrollbar = true,
+        },
+      },
+    },
+  },
+}
