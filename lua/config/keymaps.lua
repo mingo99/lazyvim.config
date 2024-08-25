64e98e1e13f6c9e187518b5dca793e3eca2e8ac9
mingo99 <2839131656@qq.com>
2024-08-26 00:35:11 +0800

    inst: toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<Esc>")
+
+vim.keymap.del("n", "<C-/>")
+vim.keymap.del("n", "<C-_>")
+vim.keymap.set("n", "<C-/>", "<cmd>ToggleTerm<cr>", { desc = "Open Toggleterm" })
+vim.keymap.set("n", "<C-_>", "<cmd>ToggleTerm<cr>", { desc = "Open Toggleterm" })
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    cmd = "ToggleTerm",
+    opts = {--[[ things you want to change go here]]
+    },
+  },
+}
