e4c427e14e6f57064c7cb26355f1c695f60bf442
mingo99 <2839131656@qq.com>
2024-09-22 21:44:32 +0800

    conf: displays the results in order according to line number

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,17 @@ return {
           width = 0.8,
           height = 0.9,
         },
+        tiebreak = function(entry1, entry2, prompt)
+          local start_pos1, _ = entry1.ordinal:find(prompt)
+          if start_pos1 then
+            local start_pos2, _ = entry2.ordinal:find(prompt)
+            if start_pos2 then
+              return start_pos1 < start_pos2
+            end
+          end
+          return false
+        end,
+        additional_args = { "--ignore-case", "--pcre2" },
       },
     },
   },
