4f6a92f079b034455c10cbe69761bf424ad96f26
mingo99 <2839131656@qq.com>
2024-10-29 23:40:32 +0800

    conf: add highlight setting of .lst

--- ftdetect/lst.lua
+++ ftdetect/lst.lua
@@ -3,15 +3,5 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   callback = function()
     vim.bo.filetype = "lst"
     vim.bo.commentstring = "# %s"
-    vim.bo.syntax = "shell"
-    -- vim.api.nvim_create_autocmd("FileType", {
-    --   pattern = ".lst",
-    --   callback = function()
-    --     vim.cmd([[
-    --       match Comment "^#.*"
-    --       match @character "^[^#].*"
-    --     ]])
-    --   end,
-    -- })
   end,
 })
