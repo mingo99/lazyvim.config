2e60bf334b39a49362a650f4167cec01bc5082e1
mingo99 <2839131656@qq.com>
2024-12-06 00:06:53 +0800

    conf: config for bigfile

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
-    { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
