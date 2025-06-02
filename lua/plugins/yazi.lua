c5e90714850c8575bfeb3dbb940cba3ba1d0d9e3
mingo99 <2839131656@qq.com>
2025-06-02 21:15:28 +0800

    yazi: tidy config items

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -4,49 +4,18 @@ return {
     event = "VeryLazy",
     keys = {
       -- 👇 in this section, choose your own keymappings!
-      {
-        "<leader>yy",
-        "<cmd>Yazi<cr>",
-        desc = "Open the file manager",
-      },
-      {
-        -- Open in the current working directory
-        "<leader>yc",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open the file manager in nvim's working directory",
-      },
-      {
-        -- NOTE: requires a version of yazi that includes
-        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-        "<leader>e",
-        "<cmd>Yazi toggle<cr>",
-        desc = "Resume the last yazi session",
-      },
+      { "<leader>yy", "<cmd>Yazi<cr>", desc = "Open the file manager" },
+      { "<leader>yc", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory" },
+      { "<leader>yt", "<cmd>Yazi toggle<cr>", desc = "Resume the last yazi session" },
     },
-
     opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = true,
-
-      -- enable these if you are using the latest version of yazi
+      floating_window_scaling_factor = 1,
+      open_for_directories = false,
       use_ya_for_events_reading = true,
       use_yazi_client_id_flag = true,
-
-      -- the type of border to use for the floating window. Can be many values,
-      -- including 'none', 'rounded', 'single', 'double', 'shadow', etc. For
-      -- more information, see :h nvim_open_win
       yazi_floating_window_border = "rounded",
-
       keymaps = {
         show_help = "<f1>",
-      },
-    },
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        hijack_netrw_behavior = "disabled",
       },
     },
   },
